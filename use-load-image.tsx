a7e7272fe1c36b4f895115d431b5139179a426ac
ArvinAbedinlou <abedinlouarvin@gmail.com>
2023-02-15 16:35:36 +0330

    initial commit

---
+++ use-load-image.tsx
@@ -0,0 +1,28 @@
+import { useEffect, DependencyList, EffectCallback } from "react";
+
+export const useLoadImage = (
+  effect: EffectCallback,
+  pictures: any[],
+  deps?: DependencyList,
+  length?: number
+) => {
+  useEffect(() => {
+    const loadImage = (image: any) => {
+      return new Promise((resolve, reject) => {
+        const loadImg = new Image();
+        loadImg.src = image.url;
+        loadImg.onload = () => {
+          resolve(image.url);
+          loadImg.onerror = (err) => reject(err);
+        };
+      });
+    };
+    Promise.allSettled(pictures.map((image: any) => loadImage(image)))
+      .then(() => {
+        if (pictures.length === length) {
+          effect();
+        }
+      })
+      .catch((err) => console.log("Failed to load images", err));
+  }, deps);
+};
